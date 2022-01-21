36bf42db18d8ddcb9e2ed26d0f95ef6ff22dfd19
Astrospence <illustrator.wood@gmail.com>
2022-01-20 20:31:31 -0700

    finished mvp

--- api/model.js
+++ api/model.js
@@ -1,7 +1,38 @@
 const db = require('../data/db-config')
 
-const getRecipeById = (recipe_id) => {
-    return db('recipes')
+const getRecipeById = async (recipe_id) => {
+    const recipeInfo = await db('recipes').where('recipes.recipe_id', recipe_id).first()
+    const stepInfo = await db('steps as s')
+        .leftJoin('ingredients as i', 's.step_id', 'i.ingredient_id')
+        .select('s.step_id', 's.step_number', 's.step_instructions', 'i.ingredient_id', 'i.ingredient_name')
+        .orderBy('s.step_number')
+
+    const response = {
+        recipe_id: recipeInfo.recipe_id,
+        recipe_name: recipeInfo.recipe_name,
+        created_at: recipeInfo.created_at,
+        steps: stepInfo.map(step => {
+            if (step.ingredient_id) {
+                return {
+                    step_id: step.step_id,
+                    step_number: step.step_number,
+                    step_instructions: step.step_instructions,
+                    ingredients: [
+                        {ingredient_id: step.ingredient_id, ingredient_name: step.ingredient_name}
+                    ]
+                }
+            } else {
+                return {
+                    step_id: step.step_id,
+                    step_number: step.step_number,
+                    step_instructions: step.step_instructions,
+                    ingredients: []
+                }
+            }
+        })
+    }
+        
+    return response
 }
 
 module.exports = {
--- api/router.js
+++ api/router.js
@@ -4,7 +4,13 @@ const Helpers = require('./model') //eslint-disable-line
 const router = express.Router()
 
 router.get('/:id', (req, res, next) => { //eslint-disable-line
-    console.log('scaffolding')
+    const { id } = req.params
+
+    Helpers.getRecipeById(id)
+        .then(recipe => {
+            res.status(200).json(recipe)
+        })
+        .catch(next)
 })
 
 module.exports = router
